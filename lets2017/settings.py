26fffe1e88b1a2436a2885c84b866623c48e9b22
suhaa18 <suhaa18@zoho.com>
2019-04-03 19:44:09 +0800

    run as daemon

--- lets2017/settings.py
+++ lets2017/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
