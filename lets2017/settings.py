19ba69d0d6e38426bd4c6432a4e7ff0abe7f8acc
suhaa18 <suhaa18@zoho.com>
2019-04-04 10:59:20 +0800

    static_root only on ubuntu

--- lets2017/settings.py
+++ lets2017/settings.py
@@ -4,12 +4,13 @@
 import os
 from platform import platform
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 if 'Ubuntu' in platform():
     DEBUG = True
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
     DEBUG = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '#rt$+y+(lkuz-aollrb8arheykm(6=gr%n%a1sqzf2_pf3)0f#'
 ALLOWED_HOSTS = ['*']
 
@@ -134,7 +135,6 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
