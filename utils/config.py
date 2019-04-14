f2a1052603bc529dd74ac0b3853751fa91b320b1
SKYDEV <sea930320@hotmail.com>
2019-04-15 00:56:20 +0800

    send message issue--1

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,6 @@
 ALIYUN_CONFIG = {
     'accessKeyId': 'LTAIuBvKDkN42lJM',
     'accessKeySecret': 'riJzYAphUq0kHr5dmfXgcKGnGxIQDO',
+    'templateCode': 'SMS_163057840',
     'from': '15640197431'
 }
