3eac5ca10b2e12e48951c097ac27e37100e20918
adampoh <adampohp79@hotmail.com>
2019-04-25 00:41:58 +0800

    2019/4/25 Get Group and Company ID

--- utils/public_fun.py
+++ utils/public_fun.py
@@ -3,7 +3,11 @@
 import os
 import time
 import random
-from project.models import *
+from account.models import *
+from group.models import *
+from group.models import AllGroups
+import json
+import code
 
 
 # 构造文件名称
@@ -32,19 +36,49 @@ def get_map(data, id):
             return d[1]
     return None
 
+
 # get group and company ID
-def getGroupAndCompanyIDsByUserID(loginType, userID):
+def getGroupByGroupManagerID(loginType, userID):
+    res = {}
     # Group Manager
     if (loginType == 2):
-        print loginType
+        # Group Manager
+        try:
+            res['login_type'] = 'G'
+            user = Tuser.objects.get(id=userID)
+            # Group ID
+            group = user.allgroups_set.all().first()
+            res['group_id'] = group.id
+            groupManagers = group.groupManagers.all()
+            groupManagersIDs = []
+            for groupManager in groupManagers:
+                groupManagersIDs.append(groupManager.id)
+            companies_group = TCompany.objects.filter(group_id=res['group_id'])
+            companies = []
+            for company_id in companies_group:
+                companies.append(company_id.id)
+            res['companies'] = companies
+            res['groupManagers'] = groupManagersIDs
+        except AttributeError as ae:
+            resp = code.get_msg(code.PERMISSION_DENIED)
+        print json.dumps(res)
+        return json.dumps(res)
+
+def getGroupByCompanyManagerID(loginType, userID):
+    res = {}
     # Company Manager
     if (loginType == 3):
-        Project.objects.filter(id__in=ids).update(is_share=0)
-        print loginType
-
-    print userID
-    return userID
-
-# get group memvers by user Id
-def getGroupMembersByUserID(userID):
-    return userID
+        # Company Manager
+        res['login_type'] = 'C'
+        user = Tuser.objects.get(id=userID)
+        # Company ID
+        company_id = user.tcompany.id
+        # Group ID
+        groupID = TCompany.objects.get(id=company_id).group.id
+        companies = []
+        companies.append(company_id)
+        # Companies Lists
+        res['companies'] = companies
+        res['group_id'] = groupID
+    print json.dumps(res)
+    return json.dumps(res)
